1fcf7bc943c0bd3652ebbd9f6a224d5d1dae94c8
Thomas Faraut <Thomas.Faraut@inra.fr>
2020-06-01 15:16:10 +0200

    adding fwd vs bckwd spiling counts

---
+++ scripts/splycing_revs/README.sh
@@ -0,0 +1,11 @@
+
+# Compute Forward and backward junction counts
+
+# Forward junction counts
+Reforma4SJtab4CompExpression.ipynb
+
+# Backward junction counts
+FormatCircCountMatrix4CompExpression.ipynb
+
+# Comparing fowrward and bakward junctoins revs (regime in frecnh)
+SplycingRevs.ipynb
